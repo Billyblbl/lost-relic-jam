da546482e10894aa904305fa93d96703c749aed8
Billyblbl <billyblbl.dev@gmail.com>
2022-06-12 00:46:00 +0200

    Coolant piping segment
    - Added Coolant piping models
    - Added Coolant pipe segment
    - Added skinned piping chain

---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#nullable enable
+
+public class Segment : MonoBehaviour {
+	public Rigidbody[] extremities = new Rigidbody[2];
+}
