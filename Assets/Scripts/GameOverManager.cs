14fb4709f9147d65f8348458f0e35bffd352e18c
Billyblbl <billyblbl.dev@gmail.com>
2022-06-19 01:40:33 +0200

    Music
    - Added main menu musics
    - Added game music
    - Added game over music
    - Fixed Scene transitions relation to build settings

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -3,29 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    [SerializeField] private GameObject gameOverOverlay;
-    [SerializeField] private RessourceSystemManager ressourceSystemMngr;
+public class GameOverManager : MonoBehaviour {
+	// Start is called before the first frame update
+	[SerializeField] private GameObject gameOverOverlay;
+	[SerializeField] private RessourceSystemManager ressourceSystemMngr;
 
-    public void OnQuitClicked()
-    {
-        Application.Quit();
-    }
+	public void OnQuitClicked() {
+		Application.Quit();
+	}
 
-    public void OnRetryClicked()
-    {
-        SceneManager.LoadScene(0, LoadSceneMode.Single);
-    }
+	public void OnRetryClicked() {
+		SceneManager.LoadScene("SpaceShip", LoadSceneMode.Single);
+	}
 
-    public void OnMainMenuClicked()
-    {
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
-    }
+	public void OnMainMenuClicked() {
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
 
-    public void StartGameOver()
-    {
-        gameOverOverlay.SetActive(true);
-    }
+	public void StartGameOver() {
+		gameOverOverlay.SetActive(true);
+	}
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    [SerializeField] private GameObject tutorialCanvas;
+public class MainMenu : MonoBehaviour {
+	[SerializeField] private GameObject tutorialCanvas;
 
-    public void OnQuitClicked()
-    {
-        Application.Quit();
-    }
+	public void OnQuitClicked() {
+		Application.Quit();
+	}
 
-    public void OnStartClicked()
-    {
-        SceneManager.LoadScene(0, LoadSceneMode.Single);
-    }
+	public void OnStartClicked() {
+		SceneManager.LoadScene("SpaceShip", LoadSceneMode.Single);
+	}
 
-    public void OnTutorialClick()
-    {
-        tutorialCanvas.SetActive(true);
-    }
+	public void OnTutorialClick() {
+		tutorialCanvas.SetActive(true);
+	}
 
-    public void OnTutorialBackClick()
-    {
-        tutorialCanvas.SetActive(false);
-    }
+	public void OnTutorialBackClick() {
+		tutorialCanvas.SetActive(false);
+	}
 }
